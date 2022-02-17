27162648dec2c2d25e631863cd1f9b86dbeaa3f7
Odin_9 <kalandarov9@gmail.com>
2022-02-18 00:41:30 +0300

    create-table
    создание таблиц и разделение на секции

---
+++ Days28_DZ_table/AppDelegate.swift
@@ -0,0 +1,33 @@
+//
+//  AppDelegate.swift
+//  Days28_DZ_table
+//
+//  Created by Yuri Kalandarov on 16.02.2022.
+//  Copyright © 2022 Yuri Kalandarov. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+//    var navController = UINavigationController()
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+
+//        let viewController = ViewController()
+//
+//        let navController = UINavigationController(rootViewController: viewController)
+//
+//        self.window = UIWindow(frame: UIScreen.main.bounds)
+//        self.window?.rootViewController = navController
+//        self.window?.backgroundColor = UIColor.black
+//        self.window?.makeKeyAndVisible()
+
+        return true
+    }
+
+}
+
---
+++ Days28_DZ_table/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  Days28_DZ_table
+//
+//  Created by Yuri Kalandarov on 16.02.2022.
+//  Copyright © 2022 Yuri Kalandarov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+//        view.backgroundColor = .systemTeal
+        title = "First Child"
+    }
+
+
+}
+
